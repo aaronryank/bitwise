c621931babe8c216061626bb6a8256b55f6ea34e
MD XF <rkcoder2002@gmail.com>
2017-10-03 18:36:13 -0700

    Functions

--- interpreter.c
+++ interpreter.c
@@ -4,17 +4,35 @@
 #include <stdint.h>
 #include <ctype.h>
 
+enum { TYPE_NEG = -1, TYPE_REG = 0, TYPE_FRAME_REG = 1, TYPE_LITERAL = 2, TYPE_LABEL = 3, TYPE_REF = 4, TYPE_ARG = 5 };
+
 int exec_cmd(char *, char *, char *, char *);
 int type(char *);
 void frame(int);
 int _atoi(char *);
+void name_function(int,char*);
+int call(int,char *,char *,char *);
 int32_t _register(char *);
 int32_t registers[1000];
 int32_t frame_registers[100][1000];
 int32_t labels[1000];
 int cur_frame;
 
-int line;
+int line, func;
+
+struct {
+  char *name;
+  char code[1000][500];
+  int lines, fline;
+  int labels[100];
+  int32_t func_registers[1000];
+} functions[200];
+int function_depth[200];
+int function_count, cur_fdepth;
+
+#define CUR_FUNC function_depth[cur_fdepth]
+#define FLINE functions[CUR_FUNC].fline
+#define FUNC_REGISTERS functions[CUR_FUNC].func_registers
 
 int main(int argc, char **argv)
 {
@@ -25,12 +43,37 @@ int main(int argc, char **argv)
         return -1;
     }
 
+    int fline = 0, cur_func = 0;
     char s[1000][500] = {0};
-    while (fgets(s[line],499,in)) {
-        if (s[line][strlen(s[line])-1] == '\n')
-            s[line][strlen(s[line])-1] = 0;
-        line++;
+    char t[500] = {0};
+    while (fgets(t,499,in)) {
+        if (strchr(t,'.') == t && strchr(t,':') == t+strlen(t)-2) {   // -2 due to newline
+            func = 1;
+            fline = 0;
+            name_function(cur_func,t);
+
+            continue;
+        }
+
+        if (t[strlen(t)-1] == '\n')
+           t[strlen(t)-1] = 0;
+
+        if (func) {
+            strcpy(functions[cur_func].code[fline],t);
+            fline++;
+        }
+        else {
+            strcpy(s[line],t);
+            line++;
+        }
+
+        if (strstr(t,"RET") == t) {
+            func = 0;
+            functions[cur_func].lines = fline - 1;
+            cur_func++;
+        }
     }
+    function_count = cur_func;
     int lines = line;
     line = 0;
 
@@ -56,22 +99,29 @@ int _register(char *index)
         return _atoi(index);
     else if (index[0] == '*')
         return frame_registers[cur_frame][_atoi(index)];
-    else if (index[0] == '@')
+    else if (index[0] == '@' && !func)
         return labels[_atoi(index)];
+    else if (index[0] == '@' && func)
+        return functions[CUR_FUNC].labels[_atoi(index)];
     else if (index[0] == '#')
         return registers[registers[_atoi(index)]];
+    else if (index[0] == '$')
+        return FUNC_REGISTERS[_atoi(index)];
     else
         return registers[atoi(index)];
 }
 
 int exec_cmd(char *cmd, char *arg1, char *arg2, char *arg3)
 {
-    int result = 0;
+    int result = 0, unrecognized = 0;
+
+    if (!cmd || !strlen(cmd))
+        return -1;
 
     if (!strcmp(cmd,"xor") || !strcmp(cmd,"XOR"))
         result = _register(arg1) ^ _register(arg2);
 
-    if (!strcmp(cmd,"and") || !strcmp(cmd,"AND"))
+    else if (!strcmp(cmd,"and") || !strcmp(cmd,"AND"))
         result = _register(arg1) & _register(arg2);
 
     else if (!strcmp(cmd,"sl") || !strcmp(cmd,"SL"))
@@ -82,11 +132,11 @@ int exec_cmd(char *cmd, char *arg1, char *arg2, char *arg3)
 
     else if (!strcmp(cmd,"mov") || !strcmp(cmd,"MOV")) {
         if (_register(arg3)) {
-            if (type(arg1) == 0)
+            if (type(arg1) == TYPE_REG)
                 registers[atoi(arg1)] = _register(arg2);
-            else if (type(arg1) == 1)
+            else if (type(arg1) == TYPE_FRAME_REG)
                 frame_registers[cur_frame][_atoi(arg1)] = _register(arg2);
-            else if (type(arg1) == 4)
+            else if (type(arg1) == TYPE_REF)
                 registers[registers[_atoi(arg1)]] = _register(arg2);
         }
         return 0;
@@ -94,25 +144,30 @@ int exec_cmd(char *cmd, char *arg1, char *arg2, char *arg3)
 
     else if (!strcmp(cmd,"jmp") || !strcmp(cmd,"JMP")) {
         if (_register(arg2)) {
-            if (type(arg1) == 3)
-                line = _register(arg1);
+            int nl = (func ? FLINE : line);
+            if (type(arg1) == TYPE_LABEL)
+                nl = _register(arg1);
             else {
-                line += _register(arg1);
+                nl += _register(arg1);
                 if (!_register(arg1))
                     exit(EXIT_SUCCESS);
             }
+            //printf(">>> jumping to line %d of %s %d\n",nl,func?"function":"code",func?CUR_FUNC:0);
+            if (func)
+                FLINE = nl;
+            else
+                line = nl;
         }
     }
 
-    else if (!strcmp(cmd,"label") || !strcmp(cmd,"LABEL"))
-        labels[_register(arg1)] = line;
-
-    else if (!strcmp(cmd,"not") || !strcmp(cmd,"NOT")) {
-        result = ~_register(arg1);
-        arg3 = strdup(arg2);
+    else if (!strcmp(cmd,"label") || !strcmp(cmd,"LABEL")) {
+        if (func)
+            functions[CUR_FUNC].labels[_register(arg1)] = FLINE;
+        else
+            labels[_register(arg1)] = line;
     }
 
-    else if (!strcmp(cmd,"lnot") || !strcmp(cmd,"LNOT")) {
+    else if (!strcmp(cmd,"not") || !strcmp(cmd,"NOT")) {
         result = ~_register(arg1);
         arg3 = strdup(arg2);
     }
@@ -129,12 +184,14 @@ int exec_cmd(char *cmd, char *arg1, char *arg2, char *arg3)
     else if (!strcmp(cmd,"in") || !strcmp(cmd,"IN")) {
         if (_register(arg2)) {
             int x = getchar();
-            if (type(arg1) == 1)
+            if (type(arg1) == TYPE_FRAME_REG)
                 frame_registers[cur_frame][_atoi(arg1)] = x;
-            else if (type(arg1) == 0)
+            else if (type(arg1) == TYPE_REG)
                 registers[atoi(arg1)] = x;
-            else if (type(arg1) == 4)
+            else if (type(arg1) == TYPE_REF)
                 registers[registers[_atoi(arg1)]] = x;
+            else if (type(arg1) == TYPE_ARG)
+                FUNC_REGISTERS[_atoi(arg1)] = x;
             result = x != -1;
         }   
     }
@@ -144,11 +201,40 @@ int exec_cmd(char *cmd, char *arg1, char *arg2, char *arg3)
             result = putchar(_register(arg1));
     }
 
+    else if (!strcmp(cmd,"outi") || !strcmp(cmd,"OUTI")) {
+        if (_register(arg2))
+            result = printf("%d ",_register(arg1));
+    }
+
     else if (!strcmp(cmd,"frame") || !strcmp(cmd,"FRAME"))
         frame(1);
 
     else if (!strcmp(cmd,"deframe") || !strcmp(cmd,"DEFRAME"))
         frame(-1);
+
+    else if (!strcmp(cmd,"ret") || !strcmp(cmd,"RET"))
+        return 0;
+
+    else if (!strcmp(cmd,"__lnot") || !strcmp(cmd,"__LNOT")) {
+        result = !_register(arg1);
+        arg3 = strdup(arg2);
+    }
+
+    else {
+        int i, called;
+        for (i = called = 0; i < function_count; i++) {
+            if (!strcmp(cmd,functions[i].name)) {
+                result = call(i,arg1,arg2,arg3);
+                called = 1;
+            }
+        }
+        if (!arg2)
+            arg2 = strdup(arg1);
+        if (!arg3)
+            arg3 = strdup(arg2);
+        if (!called)
+            unrecognized = 1;
+    }
 
     if (!arg1)
         arg1 = strdup("-1");
@@ -157,14 +243,16 @@ int exec_cmd(char *cmd, char *arg1, char *arg2, char *arg3)
     else if (!arg3)
         arg3 = strdup("-1");
 
-    if (!strcmp(cmd,"jmp") || !strcmp(cmd,"JMP") || !strcmp(cmd,"label") || !strcmp(cmd,"LABEL") || !strcmp(cmd,"frame") || !strcmp(cmd,"FRAME") || !strcmp(cmd,"deframe") || !strcmp(cmd,"DEFRAME"))
+    if (!strcmp(cmd,"jmp") || !strcmp(cmd,"JMP") || !strcmp(cmd,"label") || !strcmp(cmd,"LABEL") || !strcmp(cmd,"frame") || !strcmp(cmd,"FRAME") || !strcmp(cmd,"deframe") || !strcmp(cmd,"DEFRAME") || unrecognized)
         ;
-    else if (type(arg3) == 0)
+    else if (type(arg3) == TYPE_REG)
         registers[atoi(arg3)] = result;
-    else if (type(arg3) == 1)
+    else if (type(arg3) == TYPE_FRAME_REG)
         frame_registers[cur_frame][_atoi(arg3)] = result;
-    else if (type(arg3) == 4)
+    else if (type(arg3) == TYPE_REF)
         registers[registers[_atoi(arg3)]] = result;
+    else if (type(arg3) == TYPE_ARG)
+        FUNC_REGISTERS[_atoi(arg3)] = result;
 
     return 0;
 }
@@ -177,6 +265,57 @@ void frame(int x)
         memset(&frame_registers[cur_frame],0,sizeof(int)*1000);
         cur_frame--;
     }
+}
+
+// declaration: .foobar:\n
+// actual name: foobar
+void name_function(int idx, char *decl)
+{
+    functions[idx].name = strdup(decl);
+    char *tmp = strdup(decl);
+
+    strcpy(tmp,decl+1);
+
+    memset(functions[idx].name,0,strlen(decl)*sizeof(char));
+    strncpy(functions[idx].name,tmp,strlen(tmp)-2);
+
+    free(tmp);
+}
+
+int call(int cmd, char *arg1, char *arg2, char *arg3)
+{
+    func = 1;
+    function_depth[cur_fdepth+1] = cmd;
+    functions[function_depth[cur_fdepth+1]].func_registers[0] = cmd;
+    functions[function_depth[cur_fdepth+1]].func_registers[1] = _register(arg1);
+    functions[function_depth[cur_fdepth+1]].func_registers[2] = _register(arg2);
+    frame(1);
+    cur_fdepth++;
+
+    char *_cmd, *_arg1, *_arg2, *_arg3, *t;
+    for (FLINE = 0; FLINE <= functions[cmd].lines; FLINE++) {
+        t = strdup(functions[cmd].code[FLINE]);
+        _cmd = strtok(t," ");
+        _arg1 = strtok(NULL," ");
+        _arg2 = strtok(NULL," ");
+        _arg3 = strtok(NULL," ");
+
+        //printf("executing line %d of function %d: %s %s %s %s\n",FLINE,cmd,_cmd,_arg1,_arg2,_arg3);
+        exec_cmd(_cmd,_arg1,_arg2,_arg3);
+
+        if (!strcmp(_cmd,"ret") || !strcmp(_cmd,"RET"))
+            break;
+
+        free(t);
+    }
+
+    int retval = _register(_arg1);
+    memset(&FUNC_REGISTERS,0,sizeof(int)*1000);
+    free(t);
+    frame(-1);
+    func = CUR_FUNC = 0;
+    cur_fdepth--;
+    return retval;
 }
 
 int type(char *val)
@@ -193,6 +332,8 @@ int type(char *val)
         return 3;
     else if (*val == '#')
         return 4;
+    else if (*val == '$')
+        return 5;
     return -2;
 }
 
